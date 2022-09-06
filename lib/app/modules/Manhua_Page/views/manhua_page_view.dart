65a7a811ab499dd6063ec16fdf2b38de0a0a9866
milky_way <shin.kagami13@gmail.com>
2022-09-06 14:08:14 +0700

    add Load More in Manhua Page

--- lib/app/modules/Manhua_Page/views/manhua_page_view.dart
+++ lib/app/modules/Manhua_Page/views/manhua_page_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:my_anime_list/app/data/model/manga/manga_model.dart' as manga;
+import 'package:my_anime_list/app/routes/app_pages.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import '../controllers/manhua_page_controller.dart';
 
 class ManhuaPageView extends GetView<ManhuaPageController> {
@@ -9,16 +12,106 @@ class ManhuaPageView extends GetView<ManhuaPageController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ManhuaPageView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'ManhuaPageView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('All Manhua'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: GetBuilder<ManhuaPageController>(
+          builder: (c) {
+            return SmartRefresher(
+                controller: c.topRefresh,
+                enablePullDown: true,
+                enablePullUp: true,
+                onLoading: () => c.loadData(),
+                onRefresh: () => c.refreshData(),
+                footer: CustomFooter(
+                  builder: (context, mode) {
+                    if (mode == LoadStatus.loading) {
+                      return LoadingAnimationWidget.inkDrop(
+                          color: const Color.fromARGB(255, 6, 98, 173),
+                          size: 50);
+                    }
+                    return const SizedBox(
+                      height: 5,
+                      width: 5,
+                    );
+                  },
+                ),
+                child: GridView.builder(
+                  padding: const EdgeInsets.all(10),
+                  itemCount: c.listManhua.length,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                      childAspectRatio: 0.8,
+                      crossAxisSpacing: 40,
+                      mainAxisExtent: 200,
+                      mainAxisSpacing: 20),
+                  itemBuilder: (context, index) {
+                    manga.Manga mangas = c.listManhua[index];
+                    return GestureDetector(
+                      onTap: () {
+                        // debugPrint(c.listManhua.toString());
+                        Get.toNamed(Routes.DETAIL_MANGA, arguments: mangas);
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Stack(
+                          children: [
+                            Container(
+                              width: 200,
+                              height: 300,
+                              color: Colors.blue,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Expanded(
+                                  child: SizedBox(
+                                    width: 200,
+                                    height: 250,
+                                    child: Image.network(
+                                      mangas.images?["jpg"]?.imageUrl ??
+                                          'Kosong',
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  "${mangas.title}",
+                                  style: const TextStyle(
+                                      fontSize: 20,
+                                      overflow: TextOverflow.ellipsis,
+                                      color: Colors.white),
+                                ),
+                                Text(
+                                  "${mangas.status}",
+                                  style: const TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.amber[600],
+                                ),
+                                Text(
+                                  "${mangas.score ?? '0'}",
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      shadows: [BoxShadow(blurRadius: 1)],
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ));
+          },
+        ));
   }
 }
